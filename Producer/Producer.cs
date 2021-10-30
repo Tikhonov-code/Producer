bb70b949b9d87e94bd334a96d06984376d512761
Andrey <andrey.tikhonov@hotmail.com>
2021-10-30 09:44:32 +0300

    Add project files.

---
+++ Producer/Producer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Producer
+{
+    class Producer
+    {
+
+
+        private readonly string _storageFolder;
+        public string UrlBroker { get; set; }
+        public static int messageLifeTime = Properties.Settings.Default.MessageLifeTime;
+
+        public Producer(string uRLbroker, string storageFolder)
+        {
+            UrlBroker = uRLbroker;
+            _storageFolder = storageFolder;
+            //----------------------------------------------------------------------------------------------------
+
+            if (!Directory.Exists(_storageFolder))
+            {
+                throw new ApplicationException("error -- Storage folder setting is empty!");
+            }
+        }
+
+        internal List<string> ReadQueue()
+        {
+            List<string> result = Directory.GetFiles(_storageFolder, "*.req").ToList();
+            List<string> result0 = result.Select(fileName => Path.GetFileNameWithoutExtension(fileName)).ToList();
+
+
+            List<string> resultResponse = Directory.GetFiles(_storageFolder, "*.resp").ToList();
+            List<string> resultResponse0 = resultResponse.Select(fileName => Path.GetFileNameWithoutExtension(fileName)).ToList();
+
+            var result1 = result0.Except(resultResponse0.Where(o => result0.Contains(o))).ToList();
+            return result1;
+        }
+
+        internal void CleanQueue()
+        {
+            DateTime dt = DateTime.Now.AddMinutes(-messageLifeTime);
+            var oldfiles =  Directory.GetFiles(_storageFolder, "*.*").Where(x => new FileInfo(x).CreationTime < dt).ToList();
+            if (oldfiles.Count > 0)
+            {
+                lock (_storageFolder)
+                {
+                    try
+                    {
+                        foreach (var item in oldfiles)
+                        {
+                            File.Delete(item);
+                        }
+                        Console.WriteLine(DateTime.Now.ToLocalTime() + " CleanQueue done -- old message were dleted.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("CleanQueue ex: " + ex.Message);
+                    }
+                }
+            }
+
+        }
+
+        internal void DoResponse(List<string> fileNames)
+        {
+            foreach (var item in fileNames)
+            {
+                //1. read request
+                string fn = _storageFolder + "\\"+ item + ".req";
+                string data = string.Empty;
+                using (StreamReader sr = new StreamReader(fn))
+                {
+                    data = sr.ReadToEnd();
+                }
+                //2. write response
+                fn = _storageFolder + "\\" + item + ".resp";
+                using (StreamWriter sw = new StreamWriter(fn))
+                {
+                    sw.WriteLine("0");
+                    sw.Write(data);
+                }
+
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Producer producer = new Producer(Properties.Settings.Default.URLbroker,Properties.Settings.Default.StorageFolder);
+            List<string> fileNames;
+            int timeInterval = 3000; //ms
+            do
+            {
+                producer.CleanQueue();
+
+                fileNames = producer.ReadQueue();
+
+                if (fileNames.Count > 0) producer.DoResponse(fileNames);
+
+                fileNames.Clear();
+
+                // pause
+                Thread.Sleep(timeInterval);
+            } while (true);
+        }
+    }
+}
